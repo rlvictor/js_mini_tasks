25c34c633978faf13c40966ddaeac6dedb5ee3d7
Victor <Vityad888@yandex.ru>
2022-05-23 15:26:45 +0300

    first lesson

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 /*jshint esversion: 6*/
 // console.log("" && 6);
-let a = true,
-  b = false;
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных филмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных филмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);

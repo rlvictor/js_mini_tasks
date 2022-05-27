9cfcfb22b2e89f5689dbc4699feb2c3db2bb2e17
Victor <Vityad888@yandex.ru>
2022-05-27 15:48:13 +0300

    27.05.22

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ for (let i = 1; i < 8; i++) {
   x += "\n";
   console.log(x);
 }
+//
 
 first: for (let i = 0; i < 3; i++) {
   console.log(`1 Уровень: ${i}`);
@@ -95,6 +96,46 @@ first: for (let i = 0; i < 3; i++) {
   }
 }
 
+//
 for (let i = 1; i <= 5; i++) {
   console.log(Math.pow(i, 2));
 }
+
+//
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i <= 10; i++) {
+  arrayOfNumbers[i - 5] = i;
+}
+
+console.log(arrayOfNumbers);
+//
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   result[i] = arr[i];
+// }
+// console.log(result);
+//
+
+// const data = [5, 10, "Shopping", 20, "Homework"];
+// for (let i = 0; i < data.length; i++) {
+//   if (typeof data[i] == "string") {
+//     data[i] += " - Done";
+//   } else {
+//     data[i] *= 2;
+//   }
+// }
+// console.log(data);
+
+//Разобрать
+const data = [5, 10, "Shopping", 20, "Homework"];
+const result = [];
+
+for (let i = 1; i <= data.length; i++) {
+  result[i - 1] = data[data.length - i];
+}
+console.log(result);

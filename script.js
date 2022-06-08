a6a09c68d05168b62deb26efc7eca0e1f6e9c358
Victor <Vityad888@yandex.ru>
2022-06-08 12:36:01 +0300

    08.06.22charAt

--- script.js
+++ script.js
@@ -237,89 +237,114 @@
 
 // Объем и площадь куба
 
-function calculateVolumeAndArea(a) {
-  if (a % 1 !== 0 || a < 0 || typeof a !== "number") {
-    return "При вычислении произошла ошибка";
-  } else {
-    return (
-      "Объем куба: " + a * a * a + " , площадь всей поверхности: " + 6 * a * a
-    );
-  }
-}
+// function calculateVolumeAndArea(a) {
+//   if (a % 1 !== 0 || a < 0 || typeof a !== "number") {
+//     return "При вычислении произошла ошибка";
+//   } else {
+//     return (
+//       "Объем куба: " + a * a * a + " , площадь всей поверхности: " + 6 * a * a
+//     );
+//   }
+// }
 
 // Вычисление номера купе в вагоне
 
-function getCoupeNumber(a) {
-  if (typeof a !== "number" || a < 0 || a % 1 !== 0) {
-    return "Ошибка. Проверьте правильность введенного номера места.";
-  } else if (a == 0 || a > 36) {
-    return "Таких мест в вагоне не существует";
-  } else {
-    return (a = Math.ceil(a / 4));
-  }
-}
+// function getCoupeNumber(a) {
+//   if (typeof a !== "number" || a < 0 || a % 1 !== 0) {
+//     return "Ошибка. Проверьте правильность введенного номера места.";
+//   } else if (a == 0 || a > 36) {
+//     return "Таких мест в вагоне не существует";
+//   } else {
+//     return (a = Math.ceil(a / 4));
+//   }
+// }
 
 // Часы и минуты
 
-function getTimeFromMinutes(a) {
-  let b = a;
-  if (a < 0 || typeof a !== "number" || !Number.isInteger(a)) {
-    return `Ошибка, проверьте данные`;
-  }
-  if (a >= 60 && a < 120) {
-    b /= 60;
-    a %= 60;
-    return `Это ${Math.floor(b)} час и ${a} минут`;
-  }
-  if (a >= 120 && a < 300) {
-    b /= 60;
-    a %= 60;
-    return `Это ${Math.floor(b)} часа и ${a} минут`;
-  }
-  if (a < 60 || a >= 300) {
-    b /= 60;
-    a %= 60;
-    return `Это ${Math.floor(b)} часов и ${a} минут`;
-  }
-}
+// function getTimeFromMinutes(a) {
+//   let b = a;
+//   if (a < 0 || typeof a !== "number" || !Number.isInteger(a)) {
+//     return `Ошибка, проверьте данные`;
+//   }
+//   if (a >= 60 && a < 120) {
+//     b /= 60;
+//     a %= 60;
+//     return `Это ${Math.floor(b)} час и ${a} минут`;
+//   }
+//   if (a >= 120 && a < 300) {
+//     b /= 60;
+//     a %= 60;
+//     return `Это ${Math.floor(b)} часа и ${a} минут`;
+//   }
+//   if (a < 60 || a >= 300) {
+//     b /= 60;
+//     a %= 60;
+//     return `Это ${Math.floor(b)} часов и ${a} минут`;
+//   }
+// }
 //console.log(getTimeFromMinutes(90));
 //
 
 // 4 числа
-function findMaxNumber(a, b, c, d) {
-  if ((a, b, c, d == null || typeof (a, b, c, d) !== "number")) {
-    return 0;
-  } else {
-    return Math.max(a, b, c, d);
-  }
-}
+// function findMaxNumber(a, b, c, d) {
+//   if ((a, b, c, d == null || typeof (a, b, c, d) !== "number")) {
+//     return 0;
+//   } else {
+//     return Math.max(a, b, c, d);
+//   }
+// }
 
 //
 //Фибоначчи
 
-function fib(n) {
-  if (typeof n !== "number" || n <= 0 || !Number.isInteger(n)) {
-    return "";
-  }
-  let result = "",
-    a = 0,
-    b = 1;
-  //result += `${a} `;
-  for (let i = 0; i < n; i++) {
-    if (i + 1 === n) {
-      result += `${a}`;
-    } else {
-      result += `${a} `;
-    }
-    let c = a + b;
-    a = b;
-    b = c;
-  }
-  return result;
-}
+// function fib(n) {
+//   if (typeof n !== "number" || n <= 0 || !Number.isInteger(n)) {
+//     return "";
+//   }
+//   let result = "",
+//     a = 0,
+//     b = 1;
+//   //result += `${a} `;
+//   for (let i = 0; i < n; i++) {
+//     if (i + 1 === n) {
+//       result += `${a}`;
+//     } else {
+//       result += `${a} `;
+//     }
+//     let c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return result;
+// }
 
 //
 
+// function isEven(a) {
+//   if (a == 0) {
+//     return true;
+//   } else if (a == 1) {
+//     return false;
+//   } else if (a == -1) {
+//     return false;
+//   } else return isEven(Math.abs(a) - 2);
+// }
+// console.log(isEven(-1));
+//
+
+// CharAt
+
+function countChar(x, string) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == x) {
+      count++;
+    }
+  }
+  return count;
+}
+
+//
 let numberOfFilms;
 
 function start() {
@@ -342,7 +367,7 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных филмов?", ""),
+    const a = prompt("Один из последних просмотренных филмов?", "").trim(),
       b = prompt("На сколько оцените его?", "");
 
     if (a != null && b !== null && a != "" && b != "" && a.length < 50) {
